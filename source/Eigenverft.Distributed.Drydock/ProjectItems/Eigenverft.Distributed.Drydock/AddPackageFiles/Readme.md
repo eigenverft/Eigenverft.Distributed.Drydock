c1232d60a0f50bca780326ad4c941a3cea66c232
Eigenverft <eigenverft@outlook.com>
2025-10-05 07:26:24 +0200

    updates

---
+++ source/Eigenverft.Distributed.Drydock/ProjectItems/Eigenverft.Distributed.Drydock/AddPackageFiles/Readme.md
@@ -0,0 +1,3 @@
+# https://github.com/eigenverft/Eigenverft.Distributed.Drydock
+
+Eigenverft.Distributed.Drydock is a multi-purpose command-line .NET tool, containing a suite of helper functionalities.
---
+++ source/Eigenverft.Distributed.Drydock/ProjectItems/Eigenverft.Distributed.Drydock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eigenverft.Distributed.Drydock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
